4b559e9a4c6d326dfa01a3dddac75718708c861e
Dr. Brent Hollers <72216475+brent-hollers@users.noreply.github.com>
2026-06-16 20:27:36 -0400

    Cleaning up unneeded files

--- infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "hollers-po-tfstate-005608856189"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "po-system-terraform-locks"
-    encrypt        = true
-  }
-}
